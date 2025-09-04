0b1deae23a3890c775a09e7daf88f62925e3fc17
nhnv13122001 <nhnv13122001@gmail.com>
2025-09-04 23:30:43 +0700

    Cách đặt tên và chia thư mục

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import express from 'express'
-import userRouter from '~/users.routes'
+import usersRouter from '~/routes/users.routes'
 const app = express()
 const port = 3000
-
-app.post('/', (req, res) => {
-  res.send('hello world')
-})
-
-app.use('/users', userRouter)
+app.use(express.json())
+app.use('/users', usersRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
